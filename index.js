a4ff78dca2163dc6110dc6f042e7bf20059705ab
EGATOR <egattorr@gmail.com>
2023-07-31 10:25:42 +0000

    Website created on desktop. About to do media query

---
+++ index.js
@@ -0,0 +1,68 @@
+//============================ SERVICES TOGGLE
+const servicesButtons = document.querySelectorAll('.service__item');
+const serviceDetails = document.querySelector('.services__right');
+
+
+const getService = (category) => {
+    const details = servicesData.find(item => item.category === category);
+    serviceDetails.innerHTML = `
+        <h3>${details.title}</h3>
+        ${details.description.map(paragraph => "<p>" + paragraph + "</p>").join('')}
+    `
+}
+
+const removeActiveClass = () => {
+    servicesButtons.forEach(button => {
+        button.classList.remove('active');
+    })
+}
+
+servicesButtons.forEach(item => {
+    item.addEventListener('click', () => {
+        removeActiveClass();
+        const serviceClass = item.classList[1];
+        getService(serviceClass)
+        item.classList.add('active')
+    })
+})
+
+getService('frontend')
+
+
+
+
+
+//============================ MIXITUP (projects section)
+const containerEl = document.querySelector('.projects__container');
+var mixer = mixitup(containerEl, {
+    animation: {
+        enable: false
+    }
+});
+
+mixer.filter('*');
+
+
+
+
+
+
+
+//============================ SWIPER (testimonials section) 
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+
+    breakpoints: {
+        600: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+    });
