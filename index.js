7b5f0e20dbe5521080c648811b3e4717eda4f53f
EGATOR <egattorr@gmail.com>
2023-08-01 09:54:48 +0000

    Project done completely

--- index.js
+++ index.js
@@ -66,3 +66,68 @@ const swiper = new Swiper('.swiper', {
         }
     }
     });
+
+
+
+
+
+
+
+
+//============================ NAV TOGGLE (small screens)
+const navMenu = document.querySelector('.nav__menu')
+const navOpenBtn = document.querySelector('.nav__toggle-open')
+const navCloseBtn = document.querySelector('.nav__toggle-close')
+
+const openNavHandler = () => {
+    navMenu.style.display = 'flex';
+    navOpenBtn.style.display = 'none';
+    navCloseBtn.style.display = 'inline-block';
+}
+
+const closeNavHandler = () => {
+    navMenu.style.display = 'none';
+    navOpenBtn.style.display = 'inline-block';
+    navCloseBtn.style.display = 'none';
+}
+
+navOpenBtn.addEventListener('click', openNavHandler)
+navCloseBtn.addEventListener('click', closeNavHandler)
+
+// close nav menu on click of nav link on small screens
+const navItems = navMenu.querySelectorAll('a');
+if(window.innerWidth < 768) {
+    navItems.forEach(item => {
+        item.addEventListener('click', closeNavHandler)
+    })
+}
+
+
+
+
+
+//============================ THEME TOGGLE (light & dark mode)
+const themeBtn = document.querySelector('.nav__theme-btn');
+themeBtn.addEventListener('click', () => {
+    let bodyClass = document.body.className;
+    if(!bodyClass) {
+        bodyClass = 'dark';
+        document.body.className = bodyClass;
+        // change toggle icon
+        themeBtn.innerHTML = "<i class='uil uil-sun'></i>"
+        //save theme to local storage
+        window.localStorage.setItem('theme', bodyClass);
+    } else {
+        bodyClass = '';
+        document.body.className = bodyClass;
+        // change toggle icon
+        themeBtn.innerHTML = "<i class='uil uil-moon'></i>"
+        //save theme to local storage
+        window.localStorage.setItem('theme', bodyClass);
+    }
+})
+
+// load theme on load
+window.addEventListener('load', () => {
+    document.body.className = window.localStorage.getItem('theme');
+})
